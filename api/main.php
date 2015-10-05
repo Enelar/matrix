244373677a929fd30e0f9e2dd9ac45df2075d6aa
Kirill Berezin <enelar@exsul.net>
2015-10-05 13:50:55 +0300

    Initial matrix state. Phoxy bootstraped

---
+++ api/main.php
@@ -0,0 +1,22 @@
+<?php
+
+class main extends api
+{
+  protected function Reserve()
+  {
+    unset($this->addons['result']);
+
+    return
+    [
+      'design' => 'main/body',
+    ];
+  }
+
+  protected function Home()
+  {
+    return
+    [
+      'design' => 'main/home',
+    ];
+  }
+}
